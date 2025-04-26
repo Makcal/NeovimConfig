6331bd515d86063e01788b768253e51bc1570c89
Maxim Fomin <m.fomin@innopolis.university>
2025-04-26 13:15:03 +0300

    add Metals (Scala LSP)

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -94,5 +94,36 @@ local plugins = {
 	-- 		end,
 	-- 	},
 	-- },
+	{
+		"scalameta/nvim-metals",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		ft = { "scala", "sbt", "java" },
+		opts = function()
+			local metals_config = require("metals").bare_config()
+			metals_config.on_attach = function(client)
+				local config = require("nvchad.configs.lspconfig")
+				--- Guard against servers without the signatureHelper capability
+				if client.server_capabilities.signatureHelpProvider then
+					require("lsp-overloads").setup(client, {})
+				end
+				config.on_attach()
+			end
+
+			return metals_config
+		end,
+		config = function(self, metals_config)
+			local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = self.ft,
+				callback = function()
+					metals_config.settings.metalsBinaryPath = "metals"
+					require("metals").initialize_or_attach(metals_config)
+				end,
+				group = nvim_metals_group,
+			})
+		end,
+	},
 }
 return plugins
